d6da2d7c817b69b3b55b64365dcc3b40a6523f8b
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-15 09:59:13 -0600

    Add component directory and favicon

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Calculator from '../components/calculator/Calculator';
 import './App.css';
 
 export default function App() {
   return (
     <div className='App'>
-      <h1>react-calculator-one</h1>
+      <Calculator />
     </div>
   )
 }
---
+++ src/components/calculator/Calculator.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Calculator.css';
+
+export default function Calculator() {
+   return (
+      <div>
+         <h1>Calculator</h1>
+      </div>
+   );
+};
