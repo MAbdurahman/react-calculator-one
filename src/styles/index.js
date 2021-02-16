3809e897b20cbdd13fd145406165a10f4d1bb517
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-16 05:45:13 -0600

    Modify: Container and Screen -> cursor

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,6 +1,5 @@
 import styles from 'styled-components';
 
-
 export const Container = styles.div`
    position: relative;
    width: 400px;
@@ -11,17 +10,19 @@ export const Container = styles.div`
    grid-template-rows: minmax(120px auto) repeat(5, 100px);
    justify-content: center;
    align-content: center;
+   background-color: #808080;
    border-radius: 10px;
    // border: 1px solid #333;
    box-shadow: 2px 2px 10px #333;
+   cursor: pointer;
 
-`
+`;
 
 export const Screen = styles.div`
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
+   position: absolute;
+   left: 0;
+   top: 0;
+   width: 100%;
    height: 20%;
    padding: 10px;
    grid-column: 1 / -1;
@@ -34,5 +35,5 @@ export const Screen = styles.div`
    background-color: rgba(58, 58, 58, 1);
    border-top-left-radius: 10px;
    border-top-right-radius: 10px;
-
-`
+   cursor: text;
+`;
