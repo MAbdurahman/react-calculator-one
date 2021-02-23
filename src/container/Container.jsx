99b7665fdc658390531fe55c0270e45b882f4e31
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-23 07:51:26 -0600

    Complete buttons_feature

--- src/container/Container.jsx
+++ src/container/Container.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ButtonContainer from './button-container/ButtonContainer';
 import './Container.css';
 
 export default function Container() {
    return (
       <div className='container'>
-         
+         <ButtonContainer/>
       </div>
    );
 };
